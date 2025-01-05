9ae8a1476bb1391bbccec42590e4be67bf2b8a1f
BossOPMC <benedickrozario04@gmail.com>
2025-01-06 00:18:45 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define('DISCORD_CLIENT_ID', 'your-client-id');
+define('DISCORD_CLIENT_SECRET', 'your-client-secret');
+define('DISCORD_REDIRECT_URI', 'http://your-domain.com/auth.php');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'crashdev_panel');
+define('DB_USER', 'root');
+define('DB_PASS', 'your-database-password');
+
+define('DOCKER_IMAGE', 'crashcloud:latest');
